a252899347183519af64bddde91b78aa143c2a64
kornelijepetak <kornelije@gmail.com>
2018-08-07 05:27:41 +0200

    :bug: Fix HashCode

--- Toggl.Multivac/HashCode.cs
+++ Toggl.Multivac/HashCode.cs
@@ -25,7 +25,7 @@ namespace Toggl.Multivac
 
                 for (int i = 0; i < items.Length; i++)
                 {
-                    hash ^= items.GetHashCode() * primeNumbers[i % primeNumbers.Length];
+                    hash ^= items[i].GetHashCode() * primeNumbers[i % primeNumbers.Length];
                 }
 
                 return hash;
